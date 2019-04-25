a2249808ce21e56eda9be5e0e5c5797ceb930721
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-04-25 19:19:59 +0900

    add

--- capture-image_M100_raspi.py
+++ capture-image_M100_raspi.py
@@ -58,7 +58,7 @@ while True:
     time3 = time.strftime('%Y%m%d_H.%M.%S', time2)
     imagename = time3 + ".txt"
     ret = capture(savedir, imagename)
-    bytes = open("%s%s"%(savedir, imagename)).read()
+    #bytes = open("%s%s"%(savedir, imagename)).read()
     #bytes = open("/home/1p85m/evaluation/optical_pointing/test/fig/20181124_09.05.28/2018.11.24-09.05.48.jpg").read()
     #print("byte",len(bytes))
     #bytes += "¥n"
@@ -66,4 +66,4 @@ while True:
     #_file.write(bytes)
     #_file.close()
     #conn.send(str(len(bytes)))
-    conn.send(bytes)
+    #conn.send(bytes)
